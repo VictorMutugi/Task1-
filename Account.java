34a5fd6e71f1fcd698480c7a8ed6ca9b6a46bbed
Vic32675 <97889760+Vic32675@users.noreply.github.com>
2023-02-18 15:25:21 +0300

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,9 +1,33 @@
+import java.util.Scanner;
+
 public class Account{
  public String accountName, accountTypes;
-    public float accountBalance;
+    public float accountBalance, depositAmount, withdrawalAmount;
     public int accountNum;
-    
-    
+ 
+/* public Account(String accountName, String accountTypes,float accountBalnce, float depositAmount, float withdrawalAmount){
+  this.
+ }*/
+    Scanner input= new Scanner(System.in);
+    public void deposit(){
+     
+     System.out.print("Enter deposit amount:");
+     depositAmount= input.nextFloat();
+     accountBalance+=depositAmount;
+     System.out.print("Deposit successful!");
+    }
+ 
+ public void withdraw(){
+System.out.println("Enter withdrawal amount:");
+  withdrawalAmount= input.nextFloat();
+  if(withdrawalAmount<=accountBalance){
+  accountBalance-=withdrawalAmount;
+  System.out.println("Withdrawal successful!");
+  }
+   else {
+    System.out.println("Not enough account balance!");
+   }
+ }
     
     
     
