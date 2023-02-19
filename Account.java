8988ec89bce448b41c6733d1eaba347ff03ec478
Vic32675 <97889760+Vic32675@users.noreply.github.com>
2023-02-19 17:06:33 +0300

    Update Account.java

--- Account.java
+++ Account.java
@@ -7,6 +7,11 @@ import java.util.Scanner;
 
 public class Main {
  public static void main(String[]args){
+  Scanner myObj=new Scanner(System.in);
+   System.out.println("Your account balance is zero.Please deposit some money.");
+  Account account= new Account();
+  account.deposit();
+  
   
  }
 }
